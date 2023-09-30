b8b9c458d12f08626830b7b98f09b4710c1a6a7b
lewczuk <lewczuk@oskarwegner.pl>
2023-09-30 11:21:02 +0200

    *

--- app.py
+++ app.py
@@ -8,3 +8,11 @@ if 5 > 2:
 """
 another type of comment
 """
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+# casting
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
