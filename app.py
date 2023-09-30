25fb3d0abbf653fe8fb2a648d39a6ac4d6f08165
lewczuk <lewczuk@oskarwegner.pl>
2023-09-30 11:18:43 +0200

    *

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 print("Hello, from Python!")
+
+# if statement
+if 5 > 2:
+    print("Five is greater than two!")
+    print("second line")
+#indentation is crucial in python. have to be at least one space under statement
+"""
+another type of comment
+"""
